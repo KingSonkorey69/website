d482155cc1ca2f0a41284c0e7e7f943e1cb81079
kingsonkorey69 <you@example.com>
2020-09-26 15:52:48 +0300

    included the link to fileupload.php

--- src/fileupload.php
+++ src/fileupload.php
@@ -1,8 +1,8 @@
 <?php
 
 //require the database connection
-require_once "./database.php";
-require_once "./debug.php";
+require_once "../src/database.php";
+require_once "../src/debug.php";
 
 
 $target_dir = "assets/images/";
