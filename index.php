e4dff4783674c1712549de394bb9e3cb4870e930
kingsonkorey69 <you@example.com>
2020-09-28 13:30:11 +0300

    css changed

--- index.php
+++ index.php
@@ -112,7 +112,7 @@ include_once "./src/crud.php";
         <div class="middle">
 
             <?php foreach (getImages('book_info') as $key => $value) : ?>
-                <div class="books" id="<?php echo $value['id'] ?>">
+                <div class="samuel" id="<?php echo $value['id'] ?>">
                 <a href="info.php?q=<?php echo $value['id'] ?>>
                     <img class="book" src="./assets/images/<?php echo $value['book_image']; ?>" width="130px" height="200px">
                     <div class="paragraph">
