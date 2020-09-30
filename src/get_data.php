9b1012c2274e2ef2406e70783550d938a4c8a833
kingsonkorey69 <you@example.com>
2020-10-01 01:50:22 +0300

    changed

--- src/get_data.php
+++ src/get_data.php
@@ -2,18 +2,40 @@
 require_once('database.php');
 //
 //get the data from the server 
-if(isset($_GET["q"])){
+if (isset($_GET["q"])) {
     //get the data
-    $id= $_GET["q"];
-
-
-}else{
-//echo an error message if we dint get here
-die("you do not have the data");
-
+    $id = $_GET["q"];
+} else {
+    //echo an error message if we dint get here
+    die("you do not have the data");
 }
 //get the id from the database 
 $sql = "SELECT FROM book_info WHERE id= $id";
 $result = $conn->query($sql);
 
 $data = $result->fetchObject();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style> 
+     
+    </style>
+
+</head>
+
+<body>
+
+    <!-- This is where the book that you have clicked on will appear and will also appear with its information -->
+    <section id="animated-example" class="animated bounceInLeft info2">
+        <?php echo $data->name ?>
+
+    </section>
+</body>
+
+</html>
