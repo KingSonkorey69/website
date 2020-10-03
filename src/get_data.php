0e5758b9bbded83b66c46ae95dc6b097ddb72b05
kingsonkorey69 <you@example.com>
2020-10-03 11:57:50 +0300

    php

--- src/get_data.php
+++ src/get_data.php
@@ -11,7 +11,7 @@ if (isset($_GET['q'])) {
     die("you do not have the data");
 }
 //get the id from the database 
-$sql = "SELECT FROM book_info WHERE id= $id";
+$sql = "SELECT * FROM book_info WHERE id= $id";
 //
 $result = $conn->query($sql);
 //
