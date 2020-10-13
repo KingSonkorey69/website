e293dfb90b09e7cab504b123655e1bfa562f01ac
kingsonkorey69 <you@example.com>
2020-10-13 08:56:42 +0300

    jquery slider

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ include_once "./src/crud.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+<!-- ww -->
     <script>
         //
         //get all the books 
