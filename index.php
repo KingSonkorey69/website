d2c20aa1da880bdec9c4a1e17b6f64e846ee3bb8
kingsonkorey69 <you@example.com>
2020-10-16 22:21:22 +0300

    changed

--- index.php
+++ index.php
@@ -122,20 +122,22 @@ include_once "./src/crud.php";
         and all the books -->
 
         <div class="middle" id="<?php echo $value['id'] ?> ">
-            <div>
+            
                 <?php foreach (getImages('book_info') as $key => $value) : ?>
-                        <a href="../src/get_data.php?q=<?php echo $value['id'] ?>">
-                            <div class="paragraph">
-                                <img class="book" src="./assets/images/<?php echo $value['book_image']; ?>">
-                                <p><?php echo $value['book_title']; ?></p>
-                                <p><?php echo $value['book_upload_date']; ?></p>
-                            </div>
-                        </a>
-                        <div>
+                    <div>
+                            <a href="../src/get_data.php?q=<?php echo $value['id'] ?>">
+                                <div class="paragraph">
+                                    <img class="book" src="./assets/images/<?php echo $value['book_image']; ?>">
+                                    <p><?php echo $value['book_title']; ?></p>
+                                    <p><?php echo $value['book_upload_date']; ?></p>
+                                </div>
+                            </a>
+                            <div>
+                    </div>
                 <?php
                 endforeach;
                 ?>
-            <div>
+           
         </div>
        
         <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
