c0512397bdbca8138f258d8ec8828ba8741f4b33
kingsonkorey69 <you@example.com>
2020-10-19 23:56:16 +0300

    save the changes

--- index.php
+++ index.php
@@ -138,8 +138,8 @@ include_once "./src/crud.php";
                 endforeach;
                 ?>
             </div>
-            <script type="text/javascript" src="./slick//code.jquery.com/jquery-1.11.0.min.js"></script>
-    <script type="text/javascript" src="./slick//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+            <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script type="text/javascript" src="./slick/slick.min.js"></script>
         </div>
 
