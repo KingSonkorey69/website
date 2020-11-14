56f8e749133a178b51e926c5c0b06a5adbaca9c5
kingsonkorey69 <you@example.com>
2020-11-14 13:12:23 +0300

    save the changes

--- src/login.php
+++ src/login.php
@@ -6,7 +6,8 @@ $database = new Database();
 //intialize variables 
 $email = $password =  "";
 $email_err = $password_err = "";
-
+$email = $_POST['email'];
+$password = $_POST['password'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
