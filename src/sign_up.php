b043151d5e502053abaa858977b2b2c1d88635bd
kingsonkorey69 <you@example.com>
2021-01-12 22:27:54 +0300

    save the change in the signup.php

--- src/sign_up.php
+++ src/sign_up.php
@@ -55,8 +55,8 @@
                 <label for="psw"><b>Password:</b></label>
                 <input type="password" placeholder="Enter Password" name="psw" required>
 
-                <label for="psw-repeat"><b>Repeat Password:</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+                <!-- <label for="psw-repeat"><b>Repeat Password:</b></label>
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" required> -->
                 <label for="mobile"><b>Mobile:</b></label>
                 <input type="text" placeholder="Enter Mobile No.." name="mobile" required>
 
