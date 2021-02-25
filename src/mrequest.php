67730242c0c0f8b95ccd0af88d5c20fb08f26a06
amshel <amshelhack3r@gmail.com>
2021-02-25 22:42:30 +0300

    comment on changes needed to be implemented

--- src/mrequest.php
+++ src/mrequest.php
@@ -4,6 +4,7 @@ include_once "crud.php";
 require "../src/mpesa/details.php";
 include_once '../debug.php';
 include_once "../vendor/autoload.php";
+
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
@@ -16,50 +17,32 @@ $log->pushHandler(new StreamHandler(__DIR__ . '/../kimotho.log', Logger::INFO));
 // add records to the log
 $log->info(json_encode($_POST));
 
-        
+
 //
 //check if the requets is post 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     //
     $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
-    
-        //  
-        $d = new Details("254702129493", "200", "book_info");
-        $stk = new Stk($d);
-        //creating a new instance of the books representing the class in the crud.
-        $crud = new Crud();
-        //getting the getbooks and also giving it the id
-        $crud->getBooks($id); 
-        //
-        //select the book_info, amount, mobile from the db.
-        $sql = 
-       "SELECT
-	        book_info.book_title, book_info.book_price, users.mobile
-        FROM 
-	        book_info 
-        INNER JOIN 
-            invoice 
-    	ON
-             invoice.book_info = book_info.book_info
-        INNER JOIN 
-            users
-    	ON
-             invoice.users =  users.users
-        WHERE
-	        invoice.invoice =$id";
-        //
-        //return the result
-        $result = $this->db->query($sql);
-        //
-        echo $result->fetchAll(PDO::FETCH_ASSOC);
-        echo json_encode($result);
-       
-        //echo json_decode(file_get_contents('mpesa_request.php'));
-}else{
+
+
+    //get the id that you sent from javascript and save it in a variable called $id
+    $id; // 
+    //Create an instance of crud so as to access the method getBooks
+    $crud = new Crud();
+
+    //pass the id to the method and save the result in a variable called book
+    $book = $crud->getBooks($id);
+
+    //create a new details object. it requires 3parameters mobile->gotten from sessions,  price->gotten from getBooks and title->gotten from getBooks 
+    $purchase = new Details("user mobile", "book price", "book title"); //replace with actual values
+    $stk = new Stk($purchase);
+
+    //return the mpesa result
+    echo json_encode($stk->result);
+} else {
     http_response_code(404);
     $arr = [
-    "message" => "this is an error"
+        "message" => "this is an error"
     ];
     echo json_encode($arr);
 }
-
